1908336363a5d9447467e86f725c94554b0ebdca
Bruno <bruno.marques@escoladanuvem.org>
2025-06-24 10:41:53 -0300

    Prática 01

---
+++ pratica01/calculo_volume_caixa.py
@@ -0,0 +1,19 @@
+""" (doc strings)
+Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+Comprimento: 12 cm
+Largura: 14 cm
+Altura: 20 cm
+O programa deve calcular o volume e exibir o resultado em cm³.
+"""
+
+
+# declarando variáveis (comment strings)
+comprimento = 12
+largura = 14
+altura = 20
+
+# calculando o volume corretamente (comment strings)
+volume = comprimento * largura * altura
+
+# exibindo o resultado (comment strings)
+print(f"O volume da caixa é {volume} cm³") # f strings
